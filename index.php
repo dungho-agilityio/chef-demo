70370d86067efa142849c32b97096b7f50d37aed
Dung Ho <tanhlechi@gmail.com>
2015-03-23 14:57:18 +0700

    add index page

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @Author: Dung Ho
+ * @Date:   2015-03-23 14:56:38
+ * @Last Modified by:   Dung Ho
+ * @Last Modified time: 2015-03-23 14:57:01
+ */
+echo 'Hello motorola!!!!!! Khakhakha';
